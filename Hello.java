8b9c63cbf7cae1097c93cd4371a8a4ec47da4932
Pafax <41086673+Pafax@users.noreply.github.com>
2018-07-16 22:55:01 +0300

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Hello {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = input.nextLine();
+        System.out.println("Hello,"+ name + ",nice to meet you!");
+    }
+}
